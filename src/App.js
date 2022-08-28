a6250d3077cc1cfba437a8d6222669fc33123019
restuhanputra <restu.hanputraa@gmail.com>
2022-08-28 20:31:51 +0700

    update: class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button, Card, CardContent, Typography } from '@mui/material';
+
+import GetQuotes from './components/GetQuotes';
+
+class App extends Component {
+  state = {
+    quote: '',
+    author: '',
+  };
+
+  componentDidMount() {
+    GetQuotes().then((data) => {
+      // JSON.stringify() - convert an object to json
+      // JSON.parse() - convert json to an object
+      // console.log(data);
+      // console.log(JSON.parse(data)); // kaerna mengunakan axios jadi sudah di responsenya json
+
+      this.setState({
+        quote: data.content,
+        author: data.author,
+      });
+    });
+  }
+
+  handleClick = () => {
+    GetQuotes().then((data) => {
+      // JSON.stringify() - convert an object to json
+      // JSON.parse() - convert json to an object
+      // console.log(data);
+      // console.log(JSON.parse(data)); // kaerna mengunakan axios jadi sudah di responsenya json
+
+      this.setState({
+        quote: data.content,
+        author: data.author,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Typography variant="h2" color="#fff">
+          Random Quote App
+        </Typography>
+        <Card className="card">
+          <Typography variant="h5" paragraph={true}>
+            {this.state.quote}
+          </Typography>
+          <Typography className="margin-top" color="textSecondary">
+            {this.state.author}
+          </Typography>
+          <hr />
+          <Button
+            className="margin-top"
+            color="primary"
+            variant="outlined"
+            onClick={() => this.handleClick()}
+          >
+            Click for Quote
+          </Button>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
